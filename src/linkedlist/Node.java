7c76a51954e3856c6e782647db27bc25b73cd37b
anjanimehta <anjanimehta99@gmail.com>
2019-02-19 21:39:18 -0800

    getter+setter

--- src/linkedlist/Node.java
+++ src/linkedlist/Node.java
@@ -1,7 +1,16 @@
 package linkedlist;
 
 public class Node {
+    //commit ctrl+k
+    //push ctrl+shift+k
     private int data;
 
+    public int getData() {
+        return data;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
 
 }
